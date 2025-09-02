5380fa4b2b949f037106bfc5ff66d7d1ca58f05c
ach92130 <vosota4737@binafex.com>
2025-09-02 22:17:05 +0200

    fix upload product

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -1,11 +1,10 @@
 import { Router } from 'express';
-import { getProducts, addProduct, getProductById, uploadImage, deleteProduct } from '../controllers/productController';
+import { getProducts, addProduct, getProductById, deleteProduct } from '../controllers/productController';
 
 const router = Router();
 
 router.get('/', getProducts);
 router.get('/:id', getProductById);
 router.post('/', addProduct);
-router.post('/upload', uploadImage);
 router.delete('/:id',deleteProduct)
 export default router;
